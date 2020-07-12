f5092aea9ba9e28d341fda31794edc5468ee91d7
Harsh-git12345 <shek_23in@yahoo.com>
2020-07-12 18:11:04 +0530

    message

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,9 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.sling1 = loadImage("sprites/sling1.png")
+        this.sling2 = loadImage("sprites/sling2.png")
+        this.sling3 = loadImage("sprites/sling3.png")
     }
 
     fly(){
@@ -16,11 +19,25 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1,200,20)
+        image(this.sling2,170,20)
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            if (pointA.x<220) {
+            strokeWeight(7);
+            stroke("#301608")
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3)
+            image(this.sling3,pointA.x-30,pointA.y-10,15,30)  
+            }
+            else {
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke("#301608")
+            line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3)
+            image(this.sling3,pointA.x+25,pointA.y-10,15,30)
+            }
         }
     }
     
--- sketch.js
+++ sketch.js
@@ -39,7 +39,7 @@ function setup(){
     bird = new Bird(100,100);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:100});
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
